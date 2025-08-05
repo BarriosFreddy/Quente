808601269528092dd3a1624f7bafc9132ae6b3b6
Freddy Barrios <freddybarrios.077@gmail.com>
2025-08-04 19:41:34 -0500

    Fix

--- quente-web/vite.config.js
+++ quente-web/vite.config.js
@@ -1,22 +1,27 @@
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+
+// Estas 2 líneas reemplazan __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': resolve(__dirname, './src'),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/scss/_variables.scss";` // si tienes un archivo global
-      }
-    }
+        additionalData: `@import "@/scss/_variables.scss";`,
+      },
+    },
   },
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
 })
